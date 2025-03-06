6acd43598b39616f8c398ee98bcfbb92b60caa4c
Richard Hatherall <richard@appercept.com>
2025-03-06 15:38:02 +0000

    feat: Add Neovim Git integration

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -31,5 +31,51 @@ return {
       vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files" })
       vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live Grep" })
     end
-  }
+  },
+
+  -- Git Signs (Shows git diff in the sign column)
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup {
+        signs = {
+          add          = { text = "+" },
+          change       = { text = "~" },
+          delete       = { text = "-" },
+          topdelete    = { text = "‾" },
+          changedelete = { text = "~" },
+        },
+        current_line_blame = true, -- Show git blame inline
+      }
+    end
+  },
+
+  -- Git Commands Inside Neovim
+  { "tpope/vim-fugitive" },
+
+  -- GitHub Integration for Fugitive
+  { "tpope/vim-rhubarb" },
+
+  -- Neogit (Magit-like Git UI)
+  {
+    "TimUntersberger/neogit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true, -- Enables integration with diffview.nvim (optional)
+        }
+      }
+      vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit" })
+    end
+  },
+
+  -- Optional: DiffView for better diffs
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Open Diff View" })
+      vim.keymap.set("n", "<leader>gq", ":DiffviewClose<CR>", { desc = "Close Diff View" })
+    end
+  },
 }
