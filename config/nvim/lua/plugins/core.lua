19cc4e772190a8ec3aeea919d5458d108f9cd20f
Richard Hatherall <richard@appercept.com>
2025-03-06 17:36:31 +0000

    feat: Add formatting and linting for Ruby

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -203,4 +203,43 @@ return {
       })
     end
   },
+
+  -- None-LS: Community Fork of Null-LS for Linting & Formatting
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvimtools/none-ls-extras.nvim", -- Adds additional linters & formatters
+    },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          -- Formatters
+          -- null_ls.builtins.formatting.standardrb,   -- StandardRB for Ruby
+          null_ls.builtins.formatting.rubocop,      -- Rubocop for Ruby
+          null_ls.builtins.formatting.prettier,     -- Prettier (JS, JSON, etc.)
+
+          -- Linters
+          -- null_ls.builtins.diagnostics.standardrb,  -- StandardRB as a linter
+          null_ls.builtins.diagnostics.rubocop,     -- Rubocop as a linter
+
+          -- Code Actions
+          null_ls.builtins.code_actions.gitsigns,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            -- Auto-format on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end
+      })
+    end
+  },
 }
