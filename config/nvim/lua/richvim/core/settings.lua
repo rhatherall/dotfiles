74915c9ed8021c81ee4f691e06fa88f6d7eacffb
Richard Hatherall <richard@appercept.com>
2025-03-07 11:11:37 +0000

    feat: Add some nice settings

--- config/nvim/lua/richvim/core/settings.lua
+++ config/nvim/lua/richvim/core/settings.lua
@@ -11,3 +11,17 @@ vim.opt.cursorline = true     -- Highlight the current line
 vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 vim.opt.splitright = true  -- Open vertical splits to the left
 vim.opt.splitbelow = true   -- Open horizontal splits below
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
